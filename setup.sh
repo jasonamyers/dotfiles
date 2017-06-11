766334432068cb4becca22da836e9f3624432d28
Jason Myers <jason@jasonamyers.com>
2017-06-11 10:59:02 -0500

    Create setup.sh
    Adding setup script for starting from scratch

--- setup.sh
+++ setup.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
+# Install Homebrew
+echo -e "\n>> Installing Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"\
+
+# Install Mac apps
+echo -e "\n>> Installing Mac apps via brew cask"
+brew cask install bartender spotify 1password google-chrome iterm2 omnigraffle istat-menus pycharm atom paw docker spectacle
+
+# Install brew utils
+echo -e "\n>> Installing terminal apps via brew"
+brew install ack ctags libyaml libuv neovim/neovim/neovim sqlite tfenv redis vim go jq zsh node@6 git
+
+# Install pyenv 
+echo -e "\n>> Installing pyenv"
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+# Cloning dotfiles repo
+echo -e "\n>> Cloning dotfiles"
+git clone git@github.com:jasonamyers/dotfiles
+
+# Cloning dotfiles repo
+echo -e "\n>> Cleaning up default bash files"
+rm -rf ~/.bash*
+
 # Init symlinks for config files
 echo -e "\n>> Creating symlinks"
 for file in [a-zA-Z]* ; do
@@ -17,9 +42,26 @@ for file in [a-zA-Z]* ; do
         ln -s `pwd`/"$file" ~/."$file" && echo "  Linked ~/.$file to `pwd`/$file" || echo "  Couldn't link ~/.$file to `pwd`/$file!"
     fi
 done
-./osx-for-hackers.sh
 
-# Init vim bundles
-echo ">> Install vim bundles"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
+# Sourcing bashrc
+echo -e "\n>> Sourcing bashrc"
+source .bashrc
+
+# Installing Python 2 and 3
+echo -e "\n>> Installing Python 2 and 3"
+pyenv install 2.7.13
+pyenv install 3.6.1
+
+# Building neovim virtualenvs
+pyenv virtualenv 2.7.13 neovim2
+pyenv virtualenv 3.6.1 neovim3
+pyenv activate neovim2
+pip install neovim
+pyenv activate neovim3
+pip install neovim
+pip install flake8
+ln -s `pyenv which flake8` ~/bin/flake8
+
+# Running osx-for-hackers
+echo -e "\n>> Running osx-for-hackers"
+./osx-for-hackers.sh
